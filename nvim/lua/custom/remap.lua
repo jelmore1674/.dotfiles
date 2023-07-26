e1c5c221189ba043fc9a0580bef63f0901f274b6
Justin Elmore <jelmore@gsandf.com>
2023-07-25 22:22:55 -0500

    update config

--- nvim/lua/custom/remap.lua
+++ nvim/lua/custom/remap.lua
@@ -7,7 +7,9 @@ vim.g.mapleader = " "
 vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
 vim.keymap.set("n", "<leader>f", vim.lsp.buf.format)
 
-
+-- Move Highlight section
+vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
+vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
 
 -- Normal --
 -- Better window navigation
