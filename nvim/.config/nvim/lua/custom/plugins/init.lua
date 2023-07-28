f47fcbd8ccbb822f61a374dbed8dcf925bd704ea
Justin Elmore <jelmore@gsandf.com>
2023-07-28 15:32:26 -0500

    add vimbegood game

--- nvim/.config/nvim/lua/custom/plugins/init.lua
+++ nvim/.config/nvim/lua/custom/plugins/init.lua
@@ -3,13 +3,18 @@
 --
 -- See the kickstart.nvim README for more information
 return {
-    'MunifTanjim/prettier.nvim',
+    -- Icons
     'nvim-tree/nvim-web-devicons',
+    'MunifTanjim/prettier.nvim',
     'sindrets/diffview.nvim',
+    -- Markdown Preview
     {
         "iamcco/markdown-preview.nvim",
         build = "cd app && npm install",
         init = function() vim.g.mkdp_filetypes = { "markdown" } end,
         ft = { "markdown" },
-    }
+    },
+
+    -- VimBeGood game
+    'ThePrimeagen/vim-be-good'
 }
