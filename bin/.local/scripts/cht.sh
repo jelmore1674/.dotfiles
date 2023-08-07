8347265012d0b60f2b870c152fe734a0c45d86b3
Justin Elmore <jelmore1674@pm.me>
2023-08-06 21:24:41 -0500

    format scripts

--- bin/.local/scripts/cht.sh
+++ bin/.local/scripts/cht.sh
@@ -7,7 +7,7 @@ selected=$(echo -e "$languages\n$core_utils" | fzf)
 read -p "QUERY: " query
 
 if echo "$languages" | grep -qs $selected; then
-   tmux split-window -p 35 -h bash -c "curl cht.sh/$selected/$(echo "$query" | tr " " "+") | less"
+	tmux split-window -p 35 -h bash -c "curl cht.sh/$selected/$(echo "$query" | tr " " "+") | less"
 else
-   tmux split-window -p 35 -h bash -c "curl cht.sh/$selected~$(echo "$query" | tr " " "+") | less "
+	tmux split-window -p 35 -h bash -c "curl cht.sh/$selected~$(echo "$query" | tr " " "+") | less "
 fi
