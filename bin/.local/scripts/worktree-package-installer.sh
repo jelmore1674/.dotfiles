56a06f92e656b69a85b129e8aedaf5fdea8a89f9
Justin Elmore <jelmore1674@pm.me>
2023-08-10 00:30:52 -0500

    update files

--- bin/.local/scripts/worktree-package-installer.sh
+++ bin/.local/scripts/worktree-package-installer.sh
@@ -5,17 +5,11 @@ yarn=$(find . -maxdepth 1 -type f | grep "yarn.lock")
 npm=$(find . -maxdepth 1 -type f | grep "package-lock.json")
 pnpm=$(find . -maxdepth 1 -type f | grep "pnpm-lock.yaml")
 
-# see if node_modules already exist
-# if [ -z "$node_modules" ]; then
 # determine whether yarn or npm is used
-if [ -z "$yarn" ]; then
+if [ ! -z "$yarn" ]; then
 	yarn install
-elif [ -z "$npm"]; then
+elif [ ! -z "$npm" ]; then
 	npm install
-elif [ -z "$pnpm"]; then
+elif [ ! -z "$pnpm" ]; then
 	pnpm install
 fi
-# else
-# if the node modules are found they are installed
-#	echo "packages already installed."
-# fi
