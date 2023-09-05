32fed107fadb0ae7e7d26af6ea67767f4258aa7b
Justin Elmore <jelmore1674@pm.me>
2023-09-05 00:42:19 -0500

    add script to automate gpg setup

---
+++ bin/.local/scripts/setup-gpg-keys.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env bash
+
+# Verified Logged into 1Password
+TMP=$(mktemp)
+LOGGED_IN=$(op whoami 2>$TMP)
+err=$(cat "$TMP")
+LOGGED_IN=$(echo $err | rg "ERROR")
+
+if [[ ! -z $LOGGED_IN ]]; then
+	eval $(op signin)
+fi
+
+# get gpg keys from 1Password
+echo "Getting GPG key from 1Password"
+op document get "pgp_keys" --out-file "$HOME/pgp_keys.zip"
+
+echo "Unzipping keys"
+unzip "$HOME/pgp_keys.zip"
+
+echo "Importing keys"
+gpg --import $HOME/pgp_keys/pgp-private.asc
+gpg --import $HOME/pgp_keys/pgp-public.asc
+gpg --import-ownertrust $HOME/pgp_keys/pgp-ownertrust.asc
+
+echo "Cleaning up..."
+rm -rf "$HOME/pgp_keys" "$HOME/pgp_keys.zip"
+rm "$TMP"
