c9a4edbce083282f4b65dbfefa30d14e42366522
Justin Elmore <jelmore1674@pm.me>
2024-07-15 21:08:09 -0500

    add music module

--- tmux/.config/tmux/custom/music.sh
+++ tmux/.config/tmux/custom/music.sh
@@ -7,11 +7,11 @@ show_music() { # This function name must match the module name!
 
   index=$1 # This variable is used internally by the module loader in order to know the position of this module
 
-  icon="$(  get_tmux_option "@catppuccin_music_icon"  ""           )"
-  color="$( get_tmux_option "@catppuccin_music_color" "$thm_orange" )"
-  text="$(  get_tmux_option "@catppuccin_music_text"  #{now_playing} )"
+  icon="$(get_tmux_option "@catppuccin_music_icon" "")"
+  color="$(get_tmux_option "@catppuccin_music_color" "$thm_red")"
+  text="$(get_tmux_option "@catppuccin_music_text" " #{now_playing} ")"
 
-  module=$( build_status_module "$index" "$icon" "$color" "$text" )
+  module=$(build_status_module "$index" "$icon" "$color" "$text")
 
   echo "$module"
 }
