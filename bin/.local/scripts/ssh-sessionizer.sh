9eedb6c29d8bd0866edd53f502902194fcdbcf6e
Justin Elmore <jelmore1674@pm.me>
2024-09-23 08:32:16 -0500

    update ssh-sessionizer

--- bin/.local/scripts/ssh-sessionizer.sh
+++ bin/.local/scripts/ssh-sessionizer.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 # Get the ssh host you want to ssh to
-ssh_name=$(awk '$1 ~ /Host$/ {print $2}' <~/.ssh/config | fzf --reverse)
+ssh_name=$(awk '$1 ~ /Host$/ {print $2}' <~/.ssh/config | fzf --reverse --tmux 80%)
 
 # If a tmux session with the desired name does not already exist, create it in detached mode
 if ! tmux has-session -t="$ssh_name" 2>/dev/null; then
