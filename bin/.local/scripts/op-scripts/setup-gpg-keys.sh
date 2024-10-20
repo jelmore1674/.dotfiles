d3c00d8e3f7b875d10f898d17b05c3ba42230e22
Justin Elmore <jelmore1674@pm.me>
2024-10-20 00:22:11 -0500

    update scripts

--- bin/.local/scripts/op-scripts/setup-gpg-keys.sh
+++ bin/.local/scripts/op-scripts/setup-gpg-keys.sh
@@ -12,10 +12,11 @@ fi
 
 # get gpg keys from 1Password
 echo "Getting GPG key from 1Password"
-op document get "pgp_keys" --out-file "$HOME/pgp_keys.zip"
+#op document get "pgp_keys" --out-file "$HOME/pgp_keys.zip"
+op read --out-file "$HOME/pgp_keys.zip" "op://Private/pgp_keys/pgp_keys.zip"
 
 echo "Unzipping keys"
-unzip "$HOME/pgp_keys.zip"
+unzip "$HOME/pgp_keys.zip" -d "$HOME"
 
 echo "Importing keys"
 gpg --import $HOME/pgp_keys/pgp-private.asc
